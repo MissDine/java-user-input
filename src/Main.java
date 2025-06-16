10e959c7bd138d4b24528d5cffa90563c59c2a6f
Dine Tichi <enidnyatichi1212@gmail.com>
2025-06-16 09:31:07 +0300

    Mad libs game

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,28 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+//        Mad libs game
+Scanner scanner = new Scanner(System.in);
+        String adjective1;
+        String noun1;
+        String verb1;
+        String adjective2;
+        String adjective3;
+        System.out.println("Enter an adjective(description): ");
+        adjective1 = scanner.nextLine();
+        System.out.println("Enter a noun(Animal/person): ");
+        noun1 = scanner.nextLine();
+        System.out.println("Enter an adjective(description): ");
+        adjective2 = scanner.nextLine();
+        System.out.println("Enter an adjective(description): ");
+        verb1 = scanner.nextLine();
+        System.out.println("Enter a verb ending with ing(action) ");
+        adjective3 = scanner.nextLine();
+        System.out.println("Today I went to " + adjective1 + " zoo");
+        System.out.println("In an exhibit, I saw a " + noun1 + ".");
+        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
+        System.out.println("I was " + adjective3 + " !");
 
-//A scanner - An object that allows us to accept user input in java
-// Scanner is a built-in Java class in the java.util package.
-//It's used to read input from various sources like the keyboard, files, or strings.
-     Scanner scanner = new Scanner(System.in);
-//     Scanner - name of class
-//        scanner - name of object
-        System.out.println("Enter your name:");
-       String name = scanner.nextLine();
-//       nextline = reads a string of characters including any spaces
-//        next = does not read any spaces
-        System.out.println("Hello " + name);
-//        To read an integer
-        System.out.println("Enter your age");
-        int age = scanner.nextInt();
-        System.out.println("Hello " + name + ", you are "+ age + " years old");
-        System.out.println("What is your current GPA: ");
-        double gpa = scanner.nextDouble();
-        System.out.println("Hello " + name + " your GPA is " + gpa);
-        System.out.println("Are you a student?(true/false)");
-        boolean isStudent = scanner.nextBoolean();
-        System.out.println("Hello " + name + " it's " + isStudent + " She is a student" );
-        if(isStudent){
-            System.out.println("She is a student");
-        } else{
-            System.out.println("Not a student");
-        }
-
-        double width = 0;
-        double height = 0;
-        double area = 0;
-
-        System.out.println("Enter Width:");
-        width = scanner.nextDouble();
-        System.out.println("Enter Height:");
-        height = scanner.nextDouble();
-
-       area = height*width;
-        System.out.println("The area of the rectangle is " + area);
-     scanner.close();
-//     Closing the object to ensure that we do not have unexpected behaviour eg when reading/opening a file we will need to close it
-
-
+        scanner.close();
     }
 }
